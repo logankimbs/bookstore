298c364354120acd762c0830e9d8770d59680002
logankimball <logankimbs@gmail.com>
2022-02-14 15:19:24 -0700

    built database

---
+++ bookstore/Models/BookContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace bookstore.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options) { }
+        public DbSet<BookModel> Books { get; set; }
+    }
+}
