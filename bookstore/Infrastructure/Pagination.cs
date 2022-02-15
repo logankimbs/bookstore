c2cacd49150da4e4cc7cddb9c3f0a2815607e4bd
logankimball <logankimbs@gmail.com>
2022-02-15 12:51:55 -0700

    added pagination

---
+++ bookstore/Infrastructure/Pagination.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using bookstore.Models.ViewModels;
+
+namespace bookstore.Infrastructure
+{
+    [HtmlTargetElement("ul", Attributes = "page-blah")]
+    public class Pagination : TagHelper
+    {
+        private IUrlHelperFactory Uhf;
+
+        public Pagination(IUrlHelperFactory uhf)
+        {
+            Uhf = uhf;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext Vc { get; set; }
+        public PageInformation PageBlah { get; set; }
+        public string PageAction { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper uh = Uhf.GetUrlHelper(Vc);
+            TagBuilder final = new TagBuilder("ul");
+
+            for (int i = 1; i <= PageBlah.TotalPages; i++)
+            {
+                TagBuilder tb = new TagBuilder("a");
+                tb.Attributes["Href"] = uh.Action(PageAction, new { PageNum = i });
+                tb.Attributes["class"] = "page-link";
+                tb.InnerHtml.Append(i.ToString());
+                final.InnerHtml.AppendHtml(tb);
+            }
+
+            output.Content.AppendHtml(final.InnerHtml);
+        }
+    }
+}
---
+++ bookstore/Models/ViewModels/PageInformation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bookstore.Models.ViewModels
+{
+    public class PageInformation
+    {
+        public int NumOfProjects { get; set; }
+        public int ProjectsPerPage { get; set; }
+        public int CurrrentPage { get; set; }
+        public int TotalPages => (int)Math.Ceiling(((double)NumOfProjects / ProjectsPerPage));
+    }
+}
